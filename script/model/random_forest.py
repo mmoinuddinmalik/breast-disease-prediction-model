4f0ae7d557003f2bb5e18d50b8f5a819e4ec16b7
mmoinuddinmalik <m.moinuddinmalik@gmail.com>
2023-10-31 02:07:25 -0400

    minor updates in req and vercel config

--- script/model/random_forest.py
+++ script/model/random_forest.py
@@ -12,23 +12,23 @@ X = dataset.iloc[:, 2:32].values
 y = dataset.iloc[:, 1].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from scikit-learn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 y = labelencoder_X_1.fit_transform(y)
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from scikit-learn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+from scikit-learn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from scikit-learn.ensemble import RandomForestClassifier
+from scikit-learn.svm import SVC
+from scikit-learn.metrics import accuracy_score
 from time import time
 
 t = time()
